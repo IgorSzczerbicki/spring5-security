ed59a026c0f9b7ac25a90c86a22d620675ce3cc8
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-26 16:46:16 +0200

    Rejestracja nowych uzytkownikow

--- src/main/java/com/nauka/spring5/security/config/SecurityConfig.java
+++ src/main/java/com/nauka/spring5/security/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.nauka.spring5.security.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource securityDataSource;
+	
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+		jdbcUserDetailsManager.setDataSource(securityDataSource);
+		return jdbcUserDetailsManager;
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
+			.antMatchers("/register/**").permitAll()
 			.anyRequest().authenticated()
 		.and()
 			.formLogin()
