944de70a4c12bee5ad5efe81521b7105358be14a
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-26 09:38:47 +0200

    Uzytkownicy w DB
    Dane uzytkownikow w bazie danych z haslem w postaci jawnej
    Przechowywanie hasel w formie zaszyfrowanej

--- src/main/java/com/nauka/spring5/security/config/AppConfig.java
+++ src/main/java/com/nauka/spring5/security/config/AppConfig.java
@@ -1,16 +1,32 @@
 package com.nauka.spring5.security.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.nauka.spring5.security")
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig {
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -20,5 +36,32 @@ public class AppConfig {
 		internalResourceViewResolver.setSuffix(".jsp");
 
 		return internalResourceViewResolver;
+	}
+	
+	@Bean 
+	public DataSource securityDataSource() {
+		//utworzenie connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		//ustawienie parametrów JDBC
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+
+		logger.info(">>>>>>>>>>>>>>> " + env.getProperty("jdbc.url"));
+		logger.info(">>>>>>>>>>>>>>> " + env.getProperty("jdbc.user"));
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		//ustawienie parametrów connection pool
+		securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+		securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+		securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+		securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+		
+		return securityDataSource;
 	}
 }
--- src/main/java/com/nauka/spring5/security/config/SecurityConfig.java
+++ src/main/java/com/nauka/spring5/security/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.nauka.spring5.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+		/* teraz w DB
 		//uzytkownicy zapisywani w pamieci
 		@SuppressWarnings("deprecation")
 		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
@@ -22,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 		.withUser(userBuilder.username("jan").password("test123").roles("EMPLOYEE"))
 		.withUser(userBuilder.username("marcin").password("test123").roles("MANAGER"))
-		.withUser(userBuilder.username("olga").password("test123").roles("ADMIN"));
+		.withUser(userBuilder.username("olga").password("test123").roles("ADMIN"));*/
 	}
 
 	@Override
